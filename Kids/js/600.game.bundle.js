879359b54dc099c5cbd71d63732786198c6cbdc4
Hemel Yahya <hemelyahya@Hemels-MacBook-Pro-2.local>
2025-01-22 15:46:52 -0800

    kids game in releaes

---
+++ Kids/js/600.game.bundle.js
@@ -0,0 +1,1 @@
+self.onmessage=function(e){var a=e.data,n=0;if(a.begin){var r=performance.now();clearInterval(n),n=setInterval((function(){var e=performance.now();self.postMessage({elapsedMS:e-r}),r=e}),a.begin.delta)}a.stop&&clearInterval(n)};
